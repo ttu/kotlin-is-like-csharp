5af6f408adfb2b907756e846f59bc8ea6fbcf0a7
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2019-12-03 15:50:41 +0200

    Fix switch expression example

--- code/pattern-matching.cs
+++ code/pattern-matching.cs
@@ -3,8 +3,8 @@ var text = nb switch
 {
     int i when i < 10 => "single digit",
     10 => "double digits",
-    int i when i > 100 => "double digits",
-    int i when i > 1000 => "triple digits",
+    int i when i < 100 => "double digits",
+    int i when i < 1000 => "triple digits",
     _ => "four or more digits"
 };
 // double digits
