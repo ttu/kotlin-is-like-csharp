79d132f14954fe7230a12ef5cd4bf756b326dab6
Kent Grigo <KentGrigo@hotmail.com>
2022-11-28 12:53:27 +0100

    Move additional example down

--- code/hof-parameter.kt
+++ code/hof-parameter.kt
@@ -1,6 +1,7 @@
 fun transform(initial: String, f: (String) -> String) = f(initial)
 
 val result = transform("hello", { x -> x.toUpperCase() })
+// HELLO
+
 // Trailing lambda can be placed outside the parentheses
 val result2 = transform("hello") { x -> x.toUpperCase() }
-// HELLO
