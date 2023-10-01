1326faf5a49283dcfb9ea7c99a3120d236945e52
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2023-10-01 20:02:53 +0100

    Make Kotlin collection example clearer (#12)
    * Make Kotlin collection example clearer
    
    * Use val over var in collection example

--- code/collections.kt
+++ code/collections.kt
@@ -1,4 +1,4 @@
-var datas = listOf(
+val datas = listOf(
     SensorData(1, "A", 2.89),
     SensorData(2, "B", 12.01),
     SensorData(3, "B", 11.89),
@@ -6,12 +6,10 @@ var datas = listOf(
     SensorData(5, "A", -456.0)
 )
 
-var avgs = datas
-            .filter { it.value > -50.0 }
-            .groupBy { it.location }
-            .map { g -> 
-                Location(g.key, 
-                         g.value.map { it.value }.average()) }
-
+val avgs = datas
+    .filter { it.value > -50.0 }
+    .groupBy(SensorData::location)
+    .map { Location(it.key, it.value.map(SensorData::value).average()) }
+    
 // (location=A, value=3.0)
 // (location=B, value=11.95)
