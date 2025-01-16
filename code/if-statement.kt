0d80930fe4746b4ee7fb00a044750d29f2ec8acf
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2025-01-16 11:13:59 -0800

    Add fixes to source files over editing index.html

--- code/if-statement.kt
+++ code/if-statement.kt
@@ -1,8 +1,8 @@
 val age = 42
 
-if (age < 10) {
+if (age &lt; 10) {
     println("You're too young to watch this movie")
-} else if (age < 13) {
+} else if (age &lt; 13) {
     println("You can watch this movie with a parent")
 } else {
     println("You can watch this movie")
